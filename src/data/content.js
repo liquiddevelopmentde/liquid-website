6a7b08a30bb742f924c13b1a6edf6b6c4387fc79
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-12-18 21:39:15 +0100

    Replaced hardcoded roles

--- src/data/content.js
+++ src/data/content.js
@@ -1,41 +1,42 @@
-import { Github, Mail } from 'lucide-react';
-
 export const NAV_LINKS = ['Home', 'Projects', 'Team'];
 
 export const SOCIALS = {
-  github: "https://github.com/LiquidDevelopmentDE/",
-  email: "mailto:hello@liquid-dev.de"
+    github: "https://github.com/LiquidDevelopmentDE/",
+    email: "mailto:hello@liquid-dev.de"
 };
 
 export const TEAM_MEMBERS = [
-  {
-    name: "Felix",
-    nickname: "Flixcoo",
-    role: "FOUNDER",
-    role2: "DEVELOPER",
-    img: "https://github.com/flixcoo.png",
-    gh: "https://github.com/flixcoo",
-    email: "felix@liquid-dev.de",
-    website: "https://felixkirchner.de"
-  },
-  {
-    name: "Yannick",
-    nickname: "Gelbeinhalb",
-    role: "FOUNDER",
-    role2: "DEVELOPER",
-    img: "https://github.com/GelbEinhalb.png",
-    gh: "https://github.com/GelbEinhalb",
-    email: "yannick@liquid-dev.de",
-    website: "https://yannick-weigert.de"
-  },
-  {
-    name: "Mathis",
-    nickname: "Sneeex",
-    role: "FOUNDER",
-    role2: "DEVELOPER",
-    img: "https://github.com/mathiskir.png",
-    gh: "https://github.com/mathiskir",
-    email: "mathis@liquid-dev.de",
-    website: "https://mathiskirchner.de"
-  }
+    {
+        name: "Felix",
+        nickname: "Flixcoo",
+        roles: [0, 1],
+        img: "https://github.com/flixcoo.png",
+        gh: "https://github.com/flixcoo",
+        email: "felix@liquid-dev.de",
+        website: "https://felixkirchner.de"
+    },
+    {
+        name: "Yannick",
+        nickname: "Gelbeinhalb",
+        roles: [0, 1],
+        img: "https://github.com/GelbEinhalb.png",
+        gh: "https://github.com/GelbEinhalb",
+        email: "yannick@liquid-dev.de",
+        website: "https://yannick-weigert.de"
+    },
+    {
+        name: "Mathis",
+        nickname: "Sneeex",
+        roles: [0, 1],
+        img: "https://github.com/mathiskir.png",
+        gh: "https://github.com/mathiskir",
+        email: "mathis@liquid-dev.de",
+        website: "https://mathiskirchner.de"
+    }
+];
+
+
+export const ROLES = [
+    {id: 0, role: "FOUNDER", bgColor: "#00A3FF", borderColor: "#00A3FF", textColor: "#00A3FF"},
+    {id: 1, role: "DEVELOPER", bgColor: "#3b82f6", borderColor: "#3b82f6", textColor: "#93c5fd"},
 ];
