873cacdf0a8a20638d0db1fc5bfe60889ca4fcc1
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-12-24 13:19:17 +0100

    Added christmas effects

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,30 @@ import {Footer} from './components/Footer';
 import {Hero} from './sections/Hero';
 import {Projects} from './sections/Projects';
 import {Team} from './sections/Team';
+import Snowfall from 'react-snowfall'
 
 export default function LiquidDevelopment() {
     return (
         <div
             className="min-h-screen bg-[#0e0e0e] text-white font-sans selection:bg-[#00A3FF] selection:text-white overflow-x-hidden">
+            <div style={{
+                position: 'fixed',
+                width: '100vw',
+                height: '100vh',
+                top: 0,
+                left: 0,
+                pointerEvents: 'none',
+                zIndex: 9999,
+            }}>
+                <Snowfall
+                    snowflakeCount={80}
+                />
+            </div>
+
             <Navbar/>
+
             <Hero/>
+
             <Projects/>
             <Team/>
             <Footer/>
