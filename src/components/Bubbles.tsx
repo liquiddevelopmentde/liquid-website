2680bed235f1ef478766b5559a2fff48ebca732e
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-07-08 17:17:51 +0200

    Added push workflow

--- src/components/Bubbles.tsx
+++ src/components/Bubbles.tsx
@@ -17,30 +17,30 @@ export const Bubbles = () => {
     return (
         <div className="pointer-events-none absolute inset-0 z-0 overflow-hidden">
             {bubbles.map((bubble) => (
-                    <motion.div
-                        key={bubble.id}
-                        className="absolute rounded-full"
-                        initial={{
-                            y: '120vh',
-                            x: bubble.x,
-                            opacity: 0,
-                        }}
-                        animate={{y: '-20vh', opacity: [0, 0.7, 0]}}
-                        transition={{
-                            duration: bubble.duration,
-                            repeat: Infinity,
-                            ease: 'linear',
-                            delay: bubble.delay,
-                        }}
-                        style={{
-                            width: `${bubble.size}px`,
-                            height: `${bubble.size}px`,
-                            aspectRatio: '1/1',
-                            boxSizing: 'border-box',
-                            border: `${bubble.size / 5}px solid rgba(0, 163, 255, 0.6)`,
-                        }}
-                    />
-                ))}
+                <motion.div
+                    key={bubble.id}
+                    className="absolute rounded-full"
+                    initial={{
+                        y: '120vh',
+                        x: bubble.x,
+                        opacity: 0,
+                    }}
+                    animate={{y: '-20vh', opacity: [0, 0.7, 0]}}
+                    transition={{
+                        duration: bubble.duration,
+                        repeat: Infinity,
+                        ease: 'linear',
+                        delay: bubble.delay,
+                    }}
+                    style={{
+                        width: `${bubble.size}px`,
+                        height: `${bubble.size}px`,
+                        aspectRatio: '1/1',
+                        boxSizing: 'border-box',
+                        border: `${bubble.size / 5}px solid rgba(0, 163, 255, 0.6)`,
+                    }}
+                />
+            ))}
         </div>
     );
 };
